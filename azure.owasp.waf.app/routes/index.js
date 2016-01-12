9bddafd4637cf4bac695beae1bf23f4b39b364ec
Mike Richter <richtercloud@outlook.com>
2016-01-12 12:41:46 -0500

    Added waf app

---
+++ azure.owasp.waf.app/routes/index.js
@@ -0,0 +1,10 @@
+﻿var express = require('express');
+var router = express.Router();
+var os = require("os");
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express', hostName: os.hostname(), requestHost: req.headers.host, });
+});
+
+module.exports = router;
